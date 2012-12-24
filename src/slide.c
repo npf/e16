9f867c81d3ea60762f3af97462e9a3c5e8367f26
Kim Woelders <kim@woelders.dk>
2012-12-24 11:05:50 +0000

    Window slide code cosmetics.
    SVN revision: 81676

--- src/slide.c
+++ src/slide.c
@@ -40,13 +40,13 @@ typedef struct {
 static int
 _EobjSlideSizeTo(EObj * eo, int remaining, void *state)
 {
-   eobj_slide_params  *params = (eobj_slide_params *) state;
+   eobj_slide_params  *p = (eobj_slide_params *) state;
    int                 k = 1024 - remaining, x, y, w, h;
 
-   x = ((params->fx * (1024 - k)) + (params->tx * k)) >> 10;
-   y = ((params->fy * (1024 - k)) + (params->ty * k)) >> 10;
-   w = ((params->fw * (1024 - k)) + (params->tw * k)) >> 10;
-   h = ((params->fh * (1024 - k)) + (params->th * k)) >> 10;
+   x = (p->fx * (1024 - k) + p->tx * k) >> 10;
+   y = (p->fy * (1024 - k) + p->ty * k) >> 10;
+   w = (p->fw * (1024 - k) + p->tw * k) >> 10;
+   h = (p->fh * (1024 - k) + p->th * k) >> 10;
    EobjMoveResize(eo, x, y, w, h);
 
    return 0;
@@ -56,24 +56,23 @@ void
 EobjSlideSizeTo(EObj * eo, int fx, int fy, int tx, int ty, int fw, int fh,
 		int tw, int th, int speed)
 {
-   eobj_slide_params   params;
+   eobj_slide_params   p;
    int                 duration;
 
-   params.fx = fx;
-   params.fy = fy;
-   params.fw = fw;
-   params.fh = fh;
-   params.tx = tx;
-   params.ty = ty;
-   params.tw = tw;
-   params.th = th;
+   p.fx = fx;
+   p.fy = fy;
+   p.fw = fw;
+   p.fh = fh;
+   p.tx = tx;
+   p.ty = ty;
+   p.tw = tw;
+   p.th = th;
 
    if (speed <= 10)
       speed = 10;
    duration = 1000000 / speed;
 
-   AnimatorAdd(eo, ANIM_SLIDE, _EobjSlideSizeTo, duration, 1,
-	       sizeof(params), &params);
+   AnimatorAdd(eo, ANIM_SLIDE, _EobjSlideSizeTo, duration, 1, sizeof(p), &p);
 }
 
 /*
@@ -93,28 +92,27 @@ typedef struct {
 static int
 _EwinSlideSizeTo(EObj * eo, int remaining, void *state)
 {
-   ewin_slide_params  *params = (ewin_slide_params *) state;
+   ewin_slide_params  *p = (ewin_slide_params *) state;
    EWin               *ewin = (EWin *) eo;
    int                 k = 1024 - remaining, x, y, w, h;
 
-   x = ((params->fx * (1024 - k)) + (params->tx * k)) >> 10;
-   y = ((params->fy * (1024 - k)) + (params->ty * k)) >> 10;
-   w = ((params->fw * (1024 - k)) + (params->tw * k)) >> 10;
-   h = ((params->fh * (1024 - k)) + (params->th * k)) >> 10;
+   x = (p->fx * (1024 - k) + p->tx * k) >> 10;
+   y = (p->fy * (1024 - k) + p->ty * k) >> 10;
+   w = (p->fw * (1024 - k) + p->tw * k) >> 10;
+   h = (p->fh * (1024 - k) + p->th * k) >> 10;
 
    EwinMoveResize(ewin, x, y, w, h, MRF_KEEP_MAXIMIZED);
-   EwinShapeSet(ewin);
 
-   if (params->mouse_warp)
+   if (p->mouse_warp)
      {
 	EwinWarpTo(ewin, 1);
-	EWarpPointer(EoGetWin(ewin), params->mouse_x, params->mouse_y);
+	EWarpPointer(EoGetWin(ewin), p->mouse_x, p->mouse_y);
      }
 
    if (!remaining)
      {
 	ewin->state.sliding = 0;
-	if (params->give_focus)
+	if (p->give_focus)
 	  {
 	     FocusToEWin(ewin, FOCUS_SET);
 	  }
@@ -128,10 +126,8 @@ EwinSlideSizeTo(EWin * ewin, int tx, int ty, int tw, int th,
 		int speed, int mode, int flags)
 {
    Animator           *an;
-   ewin_slide_params   params;
+   ewin_slide_params   p;
    int                 duration, warp;
-   esound_e            start_sound = SOUND_NONE;
-   esound_e            end_sound = SOUND_NONE;
 
    warp = (flags & SLIDE_WARP) && (ewin == GetEwinPointerInClient());
 
@@ -148,37 +144,32 @@ EwinSlideSizeTo(EWin * ewin, int tx, int ty, int tw, int th,
 
    ewin->state.sliding = 1;
 
-   params.fx = EoGetX(ewin);
-   params.fy = EoGetY(ewin);
-   params.fw = ewin->client.w;
-   params.fh = ewin->client.h;
-   params.tx = tx;
-   params.ty = ty;
-   params.tw = tw;
-   params.th = th;
-   params.mode = mode;
-   params.give_focus = (flags & SLIDE_FOCUS) != 0;
-   params.mouse_warp = warp;
-   EQueryPointer(EoGetWin(ewin), &params.mouse_x, &params.mouse_y, NULL, NULL);
+   p.fx = EoGetX(ewin);
+   p.fy = EoGetY(ewin);
+   p.fw = ewin->client.w;
+   p.fh = ewin->client.h;
+   p.tx = tx;
+   p.ty = ty;
+   p.tw = tw;
+   p.th = th;
+   p.mode = mode;
+   p.give_focus = (flags & SLIDE_FOCUS) != 0;
+   p.mouse_warp = warp;
+   EQueryPointer(EoGetWin(ewin), &p.mouse_x, &p.mouse_y, NULL, NULL);
 
-   if (params.mouse_x > tw)
-      params.mouse_x = tw / 2;
-   if (params.mouse_y > th)
-      params.mouse_y = th / 2;
-
-   if (flags & SLIDE_SOUND)
-     {
-	start_sound = SOUND_WINDOW_SLIDE;
-	end_sound = SOUND_WINDOW_SLIDE_END;
-     }
+   if (p.mouse_x > tw)
+      p.mouse_x = tw / 2;
+   if (p.mouse_y > th)
+      p.mouse_y = th / 2;
 
    if (speed <= 10)
       speed = 10;
    duration = 1000000 / speed;
 
    an = AnimatorAdd((EObj *) ewin, ANIM_SLIDE, _EwinSlideSizeTo, duration, 0,
-		    sizeof(params), &params);
-   AnimatorSetSound(an, start_sound, end_sound);
+		    sizeof(p), &p);
+   if (flags & SLIDE_SOUND)
+      AnimatorSetSound(an, SOUND_WINDOW_SLIDE, SOUND_WINDOW_SLIDE_END);
 
    return an;
 }
