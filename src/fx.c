84e618b4b4331342454cf036121a2dd2328181d2
Kim Woelders <kim@woelders.dk>
2012-12-28 16:04:26 +0000

    Remove long time disabled raindrops and imagespinner fx code.
    SVN revision: 81818

--- src/fx.c
+++ src/fx.c
@@ -32,11 +32,6 @@
 #include "xwin.h"
 #include <math.h>
 
-#define E_FX_RAINDROPS          0
-#define E_FX_IMAGESPINNER       0
-
-/* FIXME - Needs cleaning up */
-
 #ifndef M_PI_2
 #define M_PI_2 (3.141592654 / 2)
 #endif
@@ -311,356 +306,10 @@ FX_Waves_Quit(void)
    FX_Waves_Desk();
 }
 
-#if E_FX_RAINDROPS
-#include "piximg.h"
-
-/****************************** RAIN DROPS **********************************/
-
-#define fx_raindrop_size 96
-#define fx_raindrop_size2 (fx_raindrop_size / 2)
-#define fx_raindrop_duration 32
-#define fx_frequency 4
-#define fx_amplitude 48
-static Win          fx_raindrops_win = NULL;
-static int          fx_raindrops_number = 4;
-static PixImg      *fx_raindrops_draw = NULL;
-
-typedef struct {
-   int                 x, y;
-   int                 count;
-   PixImg             *buf;
-} DropContext;
-
-static DropContext  fx_raindrops[4];
-
-static int
-FX_raindrops_timeout(EObj * eo __UNUSED__, int remaining __UNUSED__,
-		     void *state __UNUSED__)
-{
-   static GC           gc1 = 0, gc = 0;
-   int                 i, x, y, xx, yy;
-   int                 percent_done;
-   static char         first = 1;
-   static char         sintab[256];
-   static unsigned char disttab[fx_raindrop_size][fx_raindrop_size];
-   EObj               *bgeo;
-
-   bgeo = DeskGetBackgroundObj(DesksGetCurrent());
-
-   if (fx_raindrops_win == None)
-     {
-	XGCValues           gcv;
-
-	if (first)
-	  {
-	     int                 j;
-
-	     first = 0;
-	     for (i = 0; i < 256; i++)
-		sintab[i] =
-		   (char)(sin(((double)i) * M_PI_2 * 4 / 256) * fx_amplitude);
-	     for (j = 0; j < fx_raindrop_size; j++)
-	       {
-		  for (i = 0; i < fx_raindrop_size; i++)
-		    {
-		       xx = i - fx_raindrop_size2;
-		       yy = j - fx_raindrop_size2;
-		       disttab[i][j] =
-			  (unsigned char)sqrt((double)((xx * xx) + (yy * yy)));
-		    }
-	       }
-	  }
-
-	fx_raindrops_win = EobjGetWin(bgeo);
-
-	EXFreeGC(gc);
-	EXFreeGC(gc1);
-	gcv.subwindow_mode = IncludeInferiors;
-	gc = EXCreateGC(WinGetXwin(fx_raindrops_win), GCSubwindowMode, &gcv);
-	gc1 = EXCreateGC(WinGetXwin(fx_raindrops_win), 0L, &gcv);
-
-	fx_raindrops_draw = PixImgCreate(fx_raindrop_size, fx_raindrop_size);
-	if (!fx_raindrops_draw)
-	   return 0;
-
-	for (i = 0; i < fx_raindrops_number; i++)
-	  {
-	     fx_raindrops[i].buf =
-		PixImgCreate(fx_raindrop_size, fx_raindrop_size);
-	     if (fx_raindrops[i].buf)
-		PixImgFill(fx_raindrops[i].buf, WinGetXwin(fx_raindrops_win),
-			   fx_raindrops[i].x, fx_raindrops[i].y);
-	     if (!fx_raindrops[i].buf)
-		return 0;
-	  }
-     }
-
-   SET_GC_CLIP(bgeo, gc1);
-
-   for (i = 0; i < fx_raindrops_number; i++)
-     {
-	fx_raindrops[i].count++;
-	if (fx_raindrops[i].count == fx_raindrop_duration)
-	  {
-	     int                 j, count = 0;
-	     char                intersect = 1;
-
-	     EClearArea(fx_raindrops_win, fx_raindrops[i].x,
-			fx_raindrops[i].y, fx_raindrop_size, fx_raindrop_size);
-	     fx_raindrops[i].count = 0;
-	     while (intersect)
-	       {
-		  count++;
-		  if (count > 10240)
-		     break;
-		  intersect = 0;
-		  for (j = 0; j < fx_raindrops_number; j++)
-		    {
-		       fx_raindrops[i].x =
-			  rand() % (WinGetW(VROOT) - fx_raindrop_size);
-		       fx_raindrops[i].y =
-			  rand() % (WinGetH(VROOT) - fx_raindrop_size);
-		       if (fx_raindrops[i].x < 0)
-			  fx_raindrops[i].x = 0;
-		       else if (fx_raindrops[i].x >
-				(WinGetW(VROOT) - fx_raindrop_size))
-			  fx_raindrops[i].x = WinGetW(VROOT) - fx_raindrop_size;
-		       if (fx_raindrops[i].y < 0)
-			  fx_raindrops[i].y = 0;
-		       else if (fx_raindrops[i].y >
-				(WinGetH(VROOT) - fx_raindrop_size))
-			  fx_raindrops[i].y = WinGetH(VROOT) - fx_raindrop_size;
-		       if (i != j)
-			 {
-			    if (((fx_raindrops[i].x >= fx_raindrops[j].x)
-				 && (fx_raindrops[i].x <
-				     fx_raindrops[j].x + fx_raindrop_size)
-				 && (fx_raindrops[i].y >= fx_raindrops[j].y)
-				 && (fx_raindrops[i].y <
-				     fx_raindrops[j].y + fx_raindrop_size))
-				||
-				((fx_raindrops
-				  [i].x + fx_raindrop_size >= fx_raindrops[j].x)
-				 && (fx_raindrops[i].x + fx_raindrop_size <
-				     fx_raindrops[j].x + fx_raindrop_size)
-				 && (fx_raindrops[i].y >= fx_raindrops[j].y)
-				 && (fx_raindrops[i].y <
-				     fx_raindrops[j].y + fx_raindrop_size))
-				|| ((fx_raindrops[i].x >= fx_raindrops[j].x)
-				    && (fx_raindrops[i].x <
-					fx_raindrops[j].x + fx_raindrop_size)
-				    && (fx_raindrops[i].y + fx_raindrop_size >=
-					fx_raindrops[j].y)
-				    && (fx_raindrops[i].y + fx_raindrop_size <
-					fx_raindrops[j].y + fx_raindrop_size))
-				||
-				((fx_raindrops
-				  [i].x + fx_raindrop_size >= fx_raindrops[j].x)
-				 && (fx_raindrops[i].x + fx_raindrop_size <
-				     fx_raindrops[j].x + fx_raindrop_size)
-				 && (fx_raindrops[i].y + fx_raindrop_size >=
-				     fx_raindrops[j].y)
-				 && (fx_raindrops[i].y + fx_raindrop_size <
-				     fx_raindrops[j].y + fx_raindrop_size)))
-			       intersect = 1;
-			 }
-		    }
-	       }
-	     PixImgFill(fx_raindrops[i].buf, WinGetXwin(fx_raindrops_win),
-			fx_raindrops[i].x, fx_raindrops[i].y);
-	  }
-	percent_done =
-	   1 + ((fx_raindrops[i].count << 8) / fx_raindrop_duration);
-	for (y = 0; y < fx_raindrop_size; y++)
-	  {
-	     for (x = 0; x < fx_raindrop_size; x++)
-	       {
-		  int                 dist;
-
-		  dist = disttab[x][y];
-		  if (dist > fx_raindrop_size2)
-		     XPutPixel(fx_raindrops_draw->xim, x, y,
-			       XGetPixel(fx_raindrops[i].buf->xim, x, y));
-		  else
-		    {
-		       int                 percent;
-
-		       percent = 1 + ((dist << 8) / fx_raindrop_size2);
-		       if (percent > percent_done)
-			  XPutPixel(fx_raindrops_draw->xim, x, y,
-				    XGetPixel(fx_raindrops[i].buf->xim, x, y));
-		       else
-			 {
-			    int                 varx, vary;
-			    int                 phase, divisor, multiplier;
-
-			    phase =
-			       ((percent - percent_done) * fx_frequency) & 0xff;
-			    xx = x - fx_raindrop_size2;
-			    yy = y - fx_raindrop_size2;
-			    divisor = 1 + (dist << 8);
-			    multiplier =
-			       (int)sintab[phase] * (256 - percent_done);
-			    varx = ((-xx) * multiplier) / divisor;
-			    vary = ((-yy) * multiplier) / divisor;
-			    xx = x + varx;
-			    yy = y + vary;
-			    if (xx < 0)
-			       xx = 0;
-			    else if (xx >= fx_raindrop_size)
-			       xx = fx_raindrop_size - 1;
-			    if (yy < 0)
-			       yy = 0;
-			    else if (yy >= fx_raindrop_size)
-			       yy = fx_raindrop_size - 1;
-			    XPutPixel(fx_raindrops_draw->xim, x, y,
-				      XGetPixel(fx_raindrops[i].buf->xim, xx,
-						yy));
-			 }
-		    }
-	       }
-	  }
-	PixImgPaste(fx_raindrops_draw, WinGetXwin(fx_raindrops_win), gc1,
-		    0, 0, fx_raindrop_size, fx_raindrop_size,
-		    fx_raindrops[i].x, fx_raindrops[i].y);
-	ESync(0);
-     }
-
-   return 4;
-}
-
-static void
-FX_Raindrops_Init(const char *name __UNUSED__)
-{
-   int                 i;
-
-   fx_raindrops_win = None;
-   for (i = 0; i < fx_raindrops_number; i++)
-     {
-	fx_raindrops[i].count = rand() % fx_raindrop_duration;
-	fx_raindrops[i].x = rand() % (WinGetW(VROOT) - fx_raindrop_size);
-	fx_raindrops[i].y = rand() % (WinGetH(VROOT) - fx_raindrop_size);
-     }
-   AnimatorAdd(NULL, ANIM_FX_RAINDROPS, FX_raindrops_timeout, -1, 0, 0, NULL);
-}
-
-static void
-FX_Raindrops_Desk(void)
-{
-   fx_raindrops_win = None;
-}
-
-static void
-FX_Raindrops_Quit(void)
-{
-   int                 i;
-
-   AnimatorsDelCatAll(ANIM_FX_RAINDROPS, 0);
-   for (i = 0; i < fx_raindrops_number; i++)
-     {
-	EClearArea(fx_raindrops_win, fx_raindrops[i].x, fx_raindrops[i].y,
-		   fx_raindrop_size, fx_raindrop_size);
-	if (fx_raindrops[i].buf)
-	   PixImgDestroy(fx_raindrops[i].buf);
-	fx_raindrops[i].buf = NULL;
-     }
-   if (fx_raindrops_draw)
-      PixImgDestroy(fx_raindrops_draw);
-   fx_raindrops_draw = NULL;
-   fx_raindrops_win = None;
-}
-
-#endif /* E_FX_RAINDROPS */
-
-#if E_FX_IMAGESPINNER
-
-/****************************** IMAGESPINNER ********************************/
-
-static Win          fx_imagespinner_win = NULL;
-static int          fx_imagespinner_count = 3;
-static char        *fx_imagespinner_params = NULL;
-
-static int
-FX_imagespinner_timeout(EObj * eo __UNUSED__, int remaining __UNUSED__,
-			void *state __UNUSED__)
-{
-   char               *string = NULL;
-   EObj               *bgeo;
-
-   if (fx_imagespinner_win == None)
-     {
-	bgeo = DeskGetBackgroundObj(DesksGetCurrent());
-	fx_imagespinner_win = EobjGetWin(bgeo);
-     }
-
-#if 0				/* Don't use getword */
-/* do stuff here */
-   string = getword(fx_imagespinner_params, fx_imagespinner_count);
-   if (!string)
-     {
-	fx_imagespinner_count = 3;
-	string = getword(fx_imagespinner_params, fx_imagespinner_count);
-     }
-#endif
-
-   fx_imagespinner_count++;
-   if (string)
-     {
-	EImage             *im;
-
-	im = ThemeImageLoad(string);
-	if (im)
-	  {
-	     int                 x, y, w, h;
-
-	     EImageGetSize(im, &w, &h);
-	     sscanf(fx_imagespinner_params, "%*s %i %i ", &x, &y);
-	     x = ((WinGetW(VROOT) * x) >> 10) - ((w * x) >> 10);
-	     y = ((WinGetH(VROOT) * y) >> 10) - ((h * y) >> 10);
-	     EImageRenderOnDrawable(im, fx_imagespinner_win, None, 0,
-				    x, y, w, h);
-	     EImageFree(im);
-	  }
-	Efree(string);
-     }
-
-   return 4;
-}
-
-static void
-FX_ImageSpinner_Init(const char *name)
-{
-   fx_imagespinner_count = 3;
-   AnimatorAdd(NULL, ANIM_FX_SPINNER, FX_imagespinner_timeout, -1, 0, 0, NULL);
-   fx_imagespinner_params = Estrdup(name);
-}
-
-static void
-FX_ImageSpinner_Desk(void)
-{
-   EObj               *bgeo;
-
-   bgeo = DeskGetBackgroundObj(DesksGetCurrent());
-   fx_imagespinner_win = EobjGetWin(bgeo);
-}
-
-static void
-FX_ImageSpinner_Quit(void)
-{
-   AnimatorsDelCatAll(ANIM_FX_SPINNER, 0);
-   EClearArea(fx_imagespinner_win, 0, 0, WinGetW(VROOT), WinGetH(VROOT));
-   Efree(fx_imagespinner_params);
-   fx_imagespinner_params = NULL;
-   fx_imagespinner_win = None;
-}
-
-#endif /* E_FX_IMAGESPINNER */
-
 /****************************************************************************/
 
 #define fx_rip fx_handlers[0]
 #define fx_wav fx_handlers[1]
-#define fx_rdr fx_handlers[2]
 
 static FXHandler    fx_handlers[] = {
    {"ripples",
@@ -669,16 +318,6 @@ static FXHandler    fx_handlers[] = {
    {"waves",
     FX_Waves_Init, FX_Waves_Desk, FX_Waves_Quit,
     0, 0},
-#if E_FX_RAINDROPS
-   {"raindrops",
-    FX_Raindrops_Init, FX_Raindrops_Desk, FX_Raindrops_Quit,
-    0, 0},
-#endif
-#if E_FX_IMAGESPINNER
-   {"imagespinner",
-    FX_ImageSpinner_Init, FX_ImageSpinner_Desk, FX_ImageSpinner_Quit,
-    0, 0},
-#endif
 };
 #define N_FX_HANDLERS (sizeof(fx_handlers)/sizeof(FXHandler))
 
@@ -747,10 +386,6 @@ FxCfgFunc(void *item __UNUSED__, const char *value)
       fxh = &fx_rip;
    else if (item == &fx_wav.enabled)
       fxh = &fx_wav;
-#if E_FX_RAINDROPS
-   else if (item == &fx_rdr.enabled)
-      fxh = &fx_rdr;
-#endif
    if (!fxh)
       return;
 
@@ -791,10 +426,6 @@ FxSighan(int sig, void *prm __UNUSED__)
 static char         tmp_effect_ripples;
 static char         tmp_effect_waves;
 
-#if E_FX_RAINDROPS
-static char         tmp_effect_raindrops;
-#endif
-
 static void
 CB_ConfigureFX(Dialog * d __UNUSED__, int val, void *data __UNUSED__)
 {
@@ -803,9 +434,6 @@ CB_ConfigureFX(Dialog * d __UNUSED__, int val, void *data __UNUSED__)
 
    FX_Op(&fx_rip, tmp_effect_ripples ? FX_OP_ENABLE : FX_OP_DISABLE);
    FX_Op(&fx_wav, tmp_effect_waves ? FX_OP_ENABLE : FX_OP_DISABLE);
-#if E_FX_RAINDROPS
-   FX_Op(&fx_rdr, tmp_effect_raindrops ? FX_OP_ENABLE : FX_OP_DISABLE);
-#endif
 
    autosave();
 }
@@ -829,12 +457,6 @@ _DlgFillFx(Dialog * d __UNUSED__, DItem * table, void *data __UNUSED__)
    di = DialogAddItem(table, DITEM_CHECKBUTTON);
    DialogItemSetText(di, _("Waves"));
    DialogItemCheckButtonSetPtr(di, &tmp_effect_waves);
-#if E_FX_RAINDROPS
-   tmp_effect_raindrops = fx_rdr.enabled;
-   di = DialogAddItem(table, DITEM_CHECKBUTTON);
-   DialogItemSetText(di, _("Raindrops"));
-   DialogItemCheckButtonSetPtr(di, &tmp_effect_raindrops);
-#endif
 }
 
 const DialogDef     DlgFx = {
@@ -855,9 +477,6 @@ const DialogDef     DlgFx = {
 static const CfgItem FxCfgItems[] = {
    CFR_FUNC_BOOL(fx_handlers[0].enabled, ripples.enabled, 0, FxCfgFunc),
    CFR_FUNC_BOOL(fx_handlers[1].enabled, waves.enabled, 0, FxCfgFunc),
-#if E_FX_RAINDROPS		/* FIXME */
-   CFR_FUNC_BOOL(fx_handlers[2].enabled, raindrops.enabled, 0, FxCfgFunc),
-#endif
 };
 #define N_CFG_ITEMS (sizeof(FxCfgItems)/sizeof(CfgItem))
 
